ab37860c52f84591d9695fd232dc3ecbefd861ab
Pauloxhbh <109999895+Pauloxhbh@users.noreply.github.com>
2023-12-10 23:24:08 -0300

    Create Controlador.js

---
+++ Projeto-final/Controlador.js
@@ -0,0 +1,15 @@
+function carregarPropriedade(propriedade_id) {
+    return null;
+}
+
+function carregarContratosPorPropriedade(propriedade_id) {
+    return [];
+}
+
+function carregarManutencoesPorPropriedade(propriedade_id) {
+    return [];
+}
+
+function carregarPagamentosPorContrato(contrato_id) {
+    return [];
+}
