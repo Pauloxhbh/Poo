e8a9ffb87cd817c91f9ebb131fbc0893132d04ad
Pauloxhbh <109999895+Pauloxhbh@users.noreply.github.com>
2023-12-10 23:30:53 -0300

    Update Controlador.js

--- Projeto-final/Controlador.js
+++ Projeto-final/Controlador.js
@@ -1,15 +1,25 @@
-function carregarPropriedade(propriedade_id) {
-    return null;
+
+let propriedades = [
+    new Propriedade(1, 'Rua A, 123', 'Casa', 3, 2),
+    new Propriedade(2, 'Avenida B, 456', 'Apartamento', 2, 1),
+
+];
+function exibirListaPropriedades() {
+    let listaPropriedades = document.getElementById('lista-propriedades');
+
+    propriedades.forEach(propriedade => {
+        let propriedadeDiv = document.createElement('div');
+        propriedadeDiv.innerHTML = `
+            <h2>${propriedade.endereco}</h2>
+            <p><strong>Tipo:</strong> ${propriedade.tipo_propriedade}</p>
+            <p><strong>Quartos:</strong> ${propriedade.num_quartos}</p>
+            <p><strong>Banheiros:</strong> ${propriedade.num_banheiros}</p>
+            <hr>
+        `;
+        listaPropriedades.appendChild(propriedadeDiv);
+    });
 }
 
-function carregarContratosPorPropriedade(propriedade_id) {
-    return [];
-}
-
-function carregarManutencoesPorPropriedade(propriedade_id) {
-    return [];
-}
-
-function carregarPagamentosPorContrato(contrato_id) {
-    return [];
-}
+window.onload = function() {
+    exibirListaPropriedades();
+};
